3d6b653ee46ad4df2bd87adb1a989bbe8c65e577
John Moon <johnmoonyy@gmail.com>
2013-12-11 14:38:41 -0500

    Scaffold a quick rails application to understand Models and how the sql works

---
+++ task_list/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaskList::Application.config.secret_key_base = '3e0a1a1433cebd3f3c876c4cfceec30799a4169d4337b93ae1bb62b084a87d9df4ed044d6bcd7e4aea342ffdec5bf4782aacc23a65c92487d13145237d86e8c0'
