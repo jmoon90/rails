85ddbef8a3e2f55b0dee39387f514a84dbb0d852
John Moon <johnmoonyy@gmail.com>
2013-12-12 13:17:59 -0500

    Initial commit

---
+++ Zoo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoo::Application.config.secret_key_base = '8e0b67c0dcc07197a8556414005fd63aa520ee4022619f879e64bae3950da9a87f71e1b44c89c0bfd2eb3385ccfb04d9f1e420ec6b1f3b36a5c3937489ba2d14'
